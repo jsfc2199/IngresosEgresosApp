47017bf745291a391be2747162497cc2467b5234
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 18:29:04 -0500

    Autenticación en Login y Registro
    Se usan formularios reactivos. Se cre formulario reactivo en el registro y se añaden los controles

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
 
+  registroForm!: FormGroup
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.registroForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  crearUsuario(){
+    console.log(this.registroForm.value);
+
+  }
 }

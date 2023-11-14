d126006fcc140e5538bc27a38f99185a71a21677
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-14 17:09:23 -0500

    Estilos y Funcionalidad Básica
    Se generan los componentes que se van a utilizar y los estilos

---
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadistica',
+  templateUrl: './estadistica.component.html',
+})
+export class EstadisticaComponent {
+
+}
---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+})
+export class IngresoEgresoComponent {
+
+}

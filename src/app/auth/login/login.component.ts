ad8f261a0da29a464bd2d1db82a6e53b602faf35
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-21 21:17:41 -0500

    Autenticación en Login y Registro
    Se crea metodo del servicio para hacer login del usuario y que viaje al dashboard si todo sale bien

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  iniciarSesion() {
+    if (this.loginForm.invalid) return;
+
+    const { correo, password } = this.loginForm.value;
+    this.authService
+      .loginUsuario(correo, password)
+      .then((credenciales) => {
+        console.log(credenciales);
+        this.router.navigate(['/'])
+      })
+      .catch((err) => console.log(err));
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +9,9 @@ export class AuthService {
 
   crearUsuario(nombre: string, email: string, password: string) {
     return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  loginUsuario(email:string, password:string){
+    return signInWithEmailAndPassword(this.auth, email, password)
   }
 }

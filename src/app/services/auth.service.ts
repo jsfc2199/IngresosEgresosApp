0819059c53dbae750cbd92110d216efdc0d808ba
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-22 21:10:06 -0500

    Autenticacion en Login y Registro
    Se crea modelo de usuarios para usarlo en el servicio y postear un usuario en firebase y almacenar su info

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 import { map } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
+import { Firestore  } from '@angular/fire/firestore';
+import { collection, doc, setDoc } from '@firebase/firestore';
+
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: Auth) {}
+  constructor(private auth: Auth, private firestore: Firestore) {}
 
   //nos avisa cuando haya cambios en el estado del usuario
   initAuthListener(){
@@ -17,7 +22,20 @@ export class AuthService {
   }
 
   crearUsuario(nombre: string, email: string, password: string) {
-    return createUserWithEmailAndPassword(this.auth, email, password);
+    return createUserWithEmailAndPassword(this.auth, email, password)
+    .then(({user}) => {
+      const newUser = new Usuario(user.uid, nombre, user.email!)
+
+      // Teniendo una referencia a la coleccion de usuarios por id
+      const coleccionUsuarios = collection(this.firestore, `${user.uid}`);
+
+      // se crea referencia al documento con nombre usuarios
+      const documentRef = doc(coleccionUsuarios, 'user');
+
+      setDoc(documentRef, {...newUser}); //se debe enviar el objeto y no como instancia del objeto
+
+
+    });
   }
 
   loginUsuario(email:string, password:string){

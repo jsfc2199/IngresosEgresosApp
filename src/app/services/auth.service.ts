1ebf61b284269d6257552dbbf7ca1807b3995744
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-22 20:33:59 -0500

    Autenticacion en Login y Registro
    Se añade servicio para tener el estado del usuario

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor(private auth: Auth) {}
+
+  //nos avisa cuando haya cambios en el estado del usuario
+  initAuthListener(){
+    authState(this.auth).subscribe(user=>{
+      console.log(user?.uid);
+      console.log(user?.email);
+    })
+  }
 
   crearUsuario(nombre: string, email: string, password: string) {
     return createUserWithEmailAndPassword(this.auth, email, password);

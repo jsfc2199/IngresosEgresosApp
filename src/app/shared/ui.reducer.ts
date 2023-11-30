c98ebc308b415523e226cb3337a130f342396759
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-29 19:30:16 -0500

    Redux
    Se crean las primeras acciones y reducer

---
+++ src/app/shared/ui.reducer.ts
@@ -0,0 +1,20 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { isLoading, stopLoading } from './ui.actions';
+
+export interface State {
+    isLoading: boolean;
+}
+
+export const initialState: State = {
+  isLoading: false,
+}
+
+const _counterReducer = createReducer(initialState,
+
+    on(isLoading, state => ({ ...state, isLoading: true})),
+    on(stopLoading, state => ({ ...state, isLoading: false})),
+);
+
+export function counterReducer(state = initialState, action: Action) {
+    return _counterReducer(state, action);
+}

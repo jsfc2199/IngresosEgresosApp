dc19bcf7307212bcc5f38115bb1995fc864a2dc5
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-30 22:49:10 -0500

    Redux
    Se crean las acciones y reducer para ingresos y egresos y se actualiza el app reducer

---
+++ src/app/ingreso-egreso/ingreso-egreso-reducer.ts
@@ -0,0 +1,24 @@
+import { Action, createReducer, on } from '@ngrx/store';
+
+import { IngresoEgreso } from '../models/ingreso-egreso.model';
+import { setItems } from './ingreso-egreso.actions';
+import { unsetUser } from '../auth/auth.actions';
+
+export interface State {
+    items: IngresoEgreso[];
+}
+
+export const initialState: State = {
+   items: [],
+}
+
+const _ingresoEgresoReducer = createReducer(initialState,
+
+    on(setItems, (state, {items}) => ({ ...state, items: [...items]})),
+    on(unsetUser, (state) => ({ ...state, items: []})),
+);
+
+export function ingresoEgresoReducer(state = initialState, action: Action) {
+    return _ingresoEgresoReducer(state, action);
+}
+

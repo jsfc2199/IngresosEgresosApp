2698fed7abf897fdf39048e075a2729e99cb787b
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-11-30 20:33:07 -0500

    Redux
    Se crea modelo de ingreso egreso y se asocia formulario reactivo para poder eventualmente aplicar redux en el mismo

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
 })
-export class IngresoEgresoComponent {
+export class IngresoEgresoComponent implements OnInit{
 
+  ingresoEgresoForm: FormGroup = new FormGroup('')
+  tipoMovimiento: string = 'ingreso'
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.ingresoEgresoForm = this.formBuilder.group({
+      descripcion: ['', Validators.required],
+      monto: ['', Validators.required]
+    })
+  }
+
+  guardar(){
+    if(this.ingresoEgresoForm.invalid) return
+  }
 }

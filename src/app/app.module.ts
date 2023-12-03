8076e15269f082658b1d2567fbae87656e47da0e
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-12-03 13:43:50 -0500

    Redux Lazy Load
    Se crea el modulo de ingreso egreso que tendrá más modificaciones. Esto es el preliminar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { IngresoEgresoComponent } from './ingreso-egreso/ingreso-egreso.component';
-import { EstadisticaComponent } from './ingreso-egreso/estadistica/estadistica.component';
-import { DetalleComponent } from './ingreso-egreso/detalle/detalle.component';
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -17,31 +14,31 @@ import { environment } from '../environments/environment';
 import { StoreModule } from '@ngrx/store';
 import { appReducers } from './app.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { OrdenIngresoPipe } from './pipes/orden-ingreso.pipe';
-import { NgChartsModule } from 'ng2-charts';
+
+
 import { AuthModule } from './auth/auth.module';
 import { SharedModule } from './shared/shared.module';
+import { IngresoEgresoModule } from './ingreso-egreso/ingreso-egreso.module';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    DetalleComponent,
 
-    OrdenIngresoPipe,
   ],
   imports: [
     AuthModule,
-    SharedModule,
+
+    //se borra el sharedModule ya que solo se debe de ver cuando inicia sesion, es decir ingresoEgresoModule
+    //SharedModule,
+    
+    IngresoEgresoModule,
 
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    NgChartsModule,
+
+
+
     StoreModule.forRoot(appReducers),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
---
+++ src/app/ingreso-egreso/ingreso-egreso.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { OrdenIngresoPipe } from '../pipes/orden-ingreso.pipe';
+import { DetalleComponent } from './detalle/detalle.component';
+import { EstadisticaComponent } from './estadistica/estadistica.component';
+import { IngresoEgresoComponent } from './ingreso-egreso.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgChartsModule } from 'ng2-charts';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    DetalleComponent,
+    OrdenIngresoPipe,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgChartsModule,
+
+    SharedModule //Se importa el shared module porque ingreso egreso usa estos elemtos
+  ]
+})
+export class IngresoEgresoModule { }
